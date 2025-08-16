10d86dda75a16d8f98434a2dc36915c7047c8afe
Huzaif Ahmed <dev.huzaif@gmail.com>
2025-08-16 19:01:19 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,212 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Turning Tech Passion into Innovative Development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building an Ecommerce platform",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    // img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "TekiskyMart - Ecommerce Platform",
+    des: "Discover endless products through a seamless and engaging shopping journey at TekiskyMart.",
+    img: "/p1.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/js.png", "/node.png", "/mongodb.png"],
+    link: "/ui.tekiskymart.com",
+  },
+  {
+    id: 2,
+    title: "GearXpert - Auto Parts Ecommerce",
+    des: "Your trusted destination for premium auto parts with a sleek shopping experience.",
+    img: "/p2.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/js.png","/gsap.png"],
+    link: "/ui.gearxpert.com",
+  },
+  {
+    id: 3,
+    title: "GoGroove - Online Store",
+    des: "A modern ecommerce platform built for smooth navigation and effortless shopping.",
+    img: "/p3.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/js.png", "/node.png", "/mongodb.png"],
+    link: "/ui.gogroove.com",
+  },
+  {
+    id: 4,
+    title: "Asmak International - Seafood LLC",
+    des: "A corporate website built to showcase premium seafood exports with a professional UI.",
+    img: "/p4.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg"],
+    link: "/ui.asmak.com",
+  },
+];
+
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Huzaif was a seamless experience. His expertise and creativity turned my ideas into reality with precision and style. Highly recommended for anyone looking to build something impactful.",
+    name: "Abdul Rahman",
+    image: "/ar.webp",
+    title: "Founder of JustBuyz Ecommerce",
+  },
+  {
+    quote:
+      "Huzaif’s dedication and attention to detail really impressed me. The project was delivered on time with exceptional quality. He’s truly passionate about development and it shows in his work.",
+    name: "Mohammad Mustaqeem",
+    image: "/mm.webp",
+    title: "CEO of TechSolutions",
+  },
+  {
+    quote:
+      "From start to finish, Huzaif ensured clear communication and delivered beyond expectations. His ability to merge design with functionality makes him stand out as a developer.",
+    name: "Suleman Khan",
+    image:"/sk.webp",
+    title: "Director at Innovatech",
+  },
+];
+
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Backend Developer",
+    desc: "Built and optimized server-side applications and APIs with Node.js & Express, ensuring scalable and secure architecture.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Full-Stack Developer",
+    desc: "Delivered scalable web applications by handling both frontend and backend development, ensuring seamless integration.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Web Dev Project",
+    desc: "Led the development of a professional website for a client, from initial concept to deployment and delivery.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+
+  {
+    id: 4,
+    title: "Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
