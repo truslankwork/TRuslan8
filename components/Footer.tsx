0bf576f8c2a20b9dc3372faa3097c0991b130e32
truslankwork <truslan.kwork@gmail.com>
2025-10-15 21:41:35 +0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -38,7 +38,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Huzaif Ahmed
+          Copyright © 2022 TRuslan
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
