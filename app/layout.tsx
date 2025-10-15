aa4bc7d2cb9ee4fafea62c2df832fb2042a224e7
truslankwork <truslan.kwork@gmail.com>
2025-10-15 22:22:46 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "I'm Huzaif Ahmed",
+  title: "I'm Ruslan To'raboyev ",
   description: "Stunning Modern & Minimal Portfolio Website",
 };
 
